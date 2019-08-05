1508fff483719047d035db8632e72f5b5f38c8ea
Sara Rey <sara.rey.manchester@gmail.com>
2019-08-05 15:18:15 +0100

    xml file structure working

--- generate_xml_report.py
+++ generate_xml_report.py
@@ -1,5 +1,28 @@
 import os
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as ET
 
-output_path = ""
-output_file_name = ""
+output_path = "/Users/sararey/Documents/cruk_reporting/xml_report" #temp path for testing"
+output_file_name = "test" #should be in the format date_hospitalorgcode_patientid_sampleid
+
+def load_existing_xml(xml_file):
+    tree = ET.parse(xml_file)
+    root= tree.getroot()
+    print(root.tag) # XML rooted in this tag
+    print(root.attrib) # obtain attributes of root
+    for child in root:
+        print(child.tag, child.attrib)
+    return None
+
+
+def generate_xml():
+
+    return None
+
+
+def main():
+    generate_xml()
+    load_existing_xml(os.path.join("/Users/sararey/Documents/cruk_reporting/", "20190730 RVFAR-W014779Y-H19G5842 A1.xml"))
+
+
+if __name__ == '__main__':
+    main()
