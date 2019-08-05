eb7a46fb2f157f962fd82164094af291c80ba0c3
Sara Rey <sara.rey.manchester@gmail.com>
2019-08-05 18:11:08 +0100

    xml structure

--- generate_xml_report.py
+++ generate_xml_report.py
@@ -29,25 +29,51 @@ def generate_xml(info_dict):
     root = ET.Element("smpSample")
     root.set(NS_XSI + "noNamespaceSchemaLocation",
              'http://extranet.cancerresearchuk.org/stratmed/Shared%20Documents/smSampleXSDSchema%20V2.3.xsd')
+
     clinical_hub = ET.SubElement(root, "smClinicalHub", name=clinical_hub_name)
     patient = ET.SubElement(clinical_hub, "patient")
     ET.SubElement(patient, "organisationCode")
     ET.SubElement(patient, "localPatientIdentifier")
     ET.SubElement(patient, "localPatientIdentifier2")
-    sample = ET.SubElement(root, "smClinicalHub")
+
+    sample = ET.SubElement(root, "sample")
     clin_hub_elems = ET.SubElement(sample, "clinicalHubElements")
     ET.SubElement(clin_hub_elems, "sourceSampleIdentifier")
     ET.SubElement(clin_hub_elems, "typeOfSample")
     morph_snomed =  ET.SubElement(clin_hub_elems, "morphologySnomed")
-    morph_snomed.text = ET.CDATA("stuff")
-
+    morph_snomed.text = ET.CDATA("NA")
     ET.SubElement(clin_hub_elems, "tumourType")
     ET.SubElement(clin_hub_elems, "dateSampleSent")
     tech_hub_elems = ET.SubElement(sample, "technologyHubElements")
+    ET.SubElement(tech_hub_elems, "dateSampleReceived")
+    ET.SubElement(tech_hub_elems, "labSampleIdentifier")
+    ET.SubElement(tech_hub_elems, "reportReleaseDate")
+    banked_vol = ET.SubElement(tech_hub_elems, "volumeBankedNucleicAcid")
+    banked_vol.text = ET.CDATA("0")
+    conc_banked = ET.SubElement(tech_hub_elems, "concentrationBankedNucleicAcid")
+    conc_banked.text = ET.CDATA("0")
+    banked_loc = ET.SubElement(tech_hub_elems, "concentrationBankedNucleicAcid")
+    banked_loc.text = ET.CDATA("location")
+    banked_id = ET.SubElement(tech_hub_elems, "bankedNucleicAcididentifier")
+    banked_id.text = ET.CDATA("LabNo")
 
-
-    tech_hub = ET.SubElement(root, "smClinicalHub", name=tech_hub_name)
-
+    tech_hub = ET.SubElement(root, "smTechnologyHub", name=tech_hub_name)
+    test_res = ET.SubElement(tech_hub, "testResults")
+    # the below for all genes
+    test = ET.SubElement(test_res, "test")
+    ET.SubElement(test, "gene")
+    test_method = ET.SubElement(test, "methodOfTest")
+    test_method.text = ET.CDATA("Num")
+    test_scope = ET.SubElement(test, "scopeOfTest")
+    test_scope.text = ET.CDATA("Exon")
+    ET.SubElement(test, "dateTestResultsReleased")
+    test_result = ET.SubElement(test, "testResult")
+    test_result.text = ET.CDATA("No variant detected")
+    test_report = ET.SubElement(test, "testReport")
+    test_report.text = ET.CDATA("High confidence")
+    ET.SubElement(test, "testStatus")
+    comments = ET.SubElement(test, "comments")
+    comments.text = ET.CDATA("These results are intended for research purposes only.")
 
     edited_tree = ET.ElementTree(root)
     return edited_tree
