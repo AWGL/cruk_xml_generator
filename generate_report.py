509a181a903b4baebcc5b724d3822f32ece116d3
Sara Rey <sara.rey.manchester@gmail.com>
2019-10-01 12:00:09 +0100

    pdf awaiting clinical team feedback

--- generate_report.py
+++ generate_report.py
@@ -15,9 +15,33 @@ class GenerateReport:
                                 "withdrawn": [1.2*cm, 1.2*cm, 1.2*cm, 2.3*cm, 1.5*cm, 3.5*cm, cm, 4.6*cm]}
         self.file_name = file_name
         self.sample_dict = sample_dict
-        #column widths- if not found default columns widths for success
+        #column widths- if not found set to default columns widths for sequenced sample
         self.col_widths = self.col_widths_dict.get(report_status, self.col_widths_dict.get("success"))
 
+    def get_gene_table_data(self, table_dict, style):
+        table_data = []
+        for k, v in table_dict:
+            line_table = []
+            line_table.append(Paragraph(v.get('gene'), style))
+            line_table.append(Paragraph(k, style))
+            line_table.append(Paragraph(v.get('test_method'), style))
+            line_table.append(Paragraph(": ".join(v.get('test_scope').split(":")), style))
+            line_table.append(Paragraph(v.get('test_results'), style))
+            line_table.append(Paragraph(v.get('test_report'), style))
+            test_status = v.get('test_status')
+            line_table.append(Paragraph(test_status, style))
+            line_table.append(Paragraph(v.get('comments'), style))
+            table_data.append(line_table)
+        return table_data
+
+    def create_gene_table(self, table_data):
+        table = Table(table_data, colWidths=self.col_widths)
+        table.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+                                   ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
+                                   ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                                   ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+                                   ('ROWBACKGROUNDS', (0, 1), (-1, -1), [colors.lightgrey, colors.white])]))
+        return table
 
     def pdf_writer(self):
         template = canvas.Canvas(self.file_name, pagesize=A4, bottomup=1)
@@ -29,10 +53,7 @@ class GenerateReport:
         template.drawString(11*cm, height-(3.3*cm),
                             f"Date sample received: {self.sample_dict.get('date_sample_received')}")
 
-        #Create table of data for table format
-        print(self.sample_dict.get('genes'))
-        #TODO Abstract this out later?
-        # Create styles
+        # Create table of data for table format
         style_header = getSampleStyleSheet()["BodyText"]
         style_header.fontName = 'Helvetica'
         style_header.fontSize = 6
@@ -41,68 +62,44 @@ class GenerateReport:
         style_body.fontName = 'Helvetica'
         style_body.fontSize = 5
         style_body.textColor = colors.black
+        # Generate headers for gene and gene data tables
         headers = [Paragraph('Gene number', style_header), Paragraph('Gene name', style_header),
                        Paragraph('Test method', style_header), Paragraph('Scope of test', style_header),
                        Paragraph('Test result', style_header), Paragraph('Test report', style_header),
                        Paragraph('Test status', style_header), Paragraph('Comments', style_header)]
+
+        # Create data for first table of genes and associated data (22 genes fit on first page)
         first_table = list(islice(self.sample_dict.get('genes').items(), 22))
-        second_table = list(islice(self.sample_dict.get('genes').items(),
-                                   22, len(self.sample_dict.get('genes').items())))
-        table_data = [headers]
-        for k, v in first_table:
-            line_table = []
-            line_table.append(Paragraph(v.get('gene'), style_body))
-            line_table.append(Paragraph(k, style_body))
-            line_table.append(Paragraph(v.get('test_method'), style_body))
-            line_table.append(Paragraph(": ".join(v.get('test_scope').split(":")), style_body))
-            line_table.append(Paragraph(v.get('test_results'), style_body))
-            line_table.append(Paragraph(v.get('test_report'), style_body))
-            line_table.append(Paragraph(v.get('test_status'), style_body))
-            line_table.append(Paragraph(v.get('comments'), style_body))
-            #line_table.append(Paragraph("aaaaaaaaaa"*60, style_body))
-            table_data.append(line_table)
-        #table_data.append(PageBreak())
-
-        #TODO flowables.append(PageBreak()) [from reportlab.platypus import PageBreak]
-        table = Table(table_data, colWidths=self.col_widths)
-        table.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-                                   ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
-                                   ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
-                                   ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
-                                   ('ROWBACKGROUNDS', (0, 1), (-1, -1), [colors.lightgrey, colors.white])]))
-
-        #table._argW[4]=5*cm
+        table_data = self.get_gene_table_data(first_table, style_body)
+        # Count number of failed samples- all failed samples are stored as a 3 in the second from last column
+        num_fails = len([count[-2].text for count in table_data if count[-2].text == "3"])
+        # Add headers to table (beginning)
+        table_data.insert(0, headers)
+        # Draw table
+        table = self.create_gene_table(table_data)
+        # Draw table on to template
         table.wrapOn(template, width, height)
         table.drawOn(template, 2*cm, 1.5*cm)
         template.setFont("Helvetica", 8)
         template.drawString(width-(4*cm), cm, "Page 1 of 2")
-
         template.showPage()
 
-        # Multiple pages
-        table_data = [headers]
-        for k, v in second_table:
-            line_table = []
-            line_table.append(Paragraph(v.get('gene'), style_body))
-            line_table.append(Paragraph(k, style_body))
-            line_table.append(Paragraph(v.get('test_method'), style_body))
-            line_table.append(Paragraph(": ".join(v.get('test_scope').split(":")), style_body))
-            line_table.append(Paragraph(v.get('test_results'), style_body))
-            line_table.append(Paragraph(v.get('test_report'), style_body))
-            line_table.append(Paragraph(v.get('test_status'), style_body))
-            line_table.append(Paragraph(v.get('comments'), style_body))
-            table_data.append(line_table)
-
-        table = Table(table_data, colWidths=self.col_widths)
-        table.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-                                   ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
-                                   ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
-                                   ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
-                                   ('ROWBACKGROUNDS', (0, 1), (-1, -1), [colors.lightgrey, colors.white])]))
-
+        # Split table over multiple pages
+        # Create data for second table of genes and associated data
+        second_table = list(islice(self.sample_dict.get('genes').items(),
+                                   22, len(self.sample_dict.get('genes').items())))
+        table_data = self.get_gene_table_data(second_table, style_body)
+        # Count number of failed samples- all failed samples are stored as a 3 in the second from last column
+        num_fails += len([count[-2].text for count in table_data if count[-2].text == "3"])
+        # Add headers to table (beginning)
+        table_data.insert(0, headers)
+        # Draw table
+        table = self.create_gene_table(table_data)
+        # Draw table on to template
         table.wrapOn(template, width, height)
         table.drawOn(template, 2*cm, 4.2*cm)
 
+        # Create data for table of people checking this report
         table_data = [["", "", "Date"],
                       ["Checker 1", self.sample_dict.get('reported_by_1'), self.sample_dict.get('date_reported_1')],
                       ["Checker 2", self.sample_dict.get('reported_by_2'), self.sample_dict.get('date_reported_2')],
@@ -113,15 +110,17 @@ class GenerateReport:
                                    ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
                                    ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
                                    ('ROWBACKGROUNDS', (0, 1), (-1, -1), [colors.white])]))
+        # Draw table on to template
         table.wrapOn(template, width, height)
         table.drawOn(template, 2*cm, 1.4*cm)
 
-        template.drawString(8 * cm, 3.5 * cm, f"Gene fails: {0}") #TODO Obtain this value
-
+        # Number of failed genes for this sample
+        template.drawString(8 * cm, 3.5 * cm, f"Gene fails: {num_fails}")
         template.setFont("Helvetica", 8)
         template.drawString(width-(4*cm), cm, "Page 2 of 2")
         template.showPage()
         template.save()
+        return (f"PDF report {self.file_name} generated")
 
         '''
         template.drawString(2*cm, 2*cm,f"Checker 1 {self.sample_dict.get('reported_by_1')}")
