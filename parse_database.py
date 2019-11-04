3f42cc8bf6fc95e2b66de2944e1123404fb70a30
Sara Rey <sara.rey.manchester@gmail.com>
2019-11-04 17:13:19 +0000

    testing started

--- parse_database.py
+++ parse_database.py
@@ -12,25 +12,29 @@ class ParseDatabase:
         db = db_wb.sheet_by_name(worksheet)
         return db
 
-    def open_database_as_dataframe(self, sheet, header=0): # Column headers are 1st row. Data starts after this.
+    def open_database_as_dataframe(self, sheet, header=0):  # Column headers are 1st row. Data starts after this.
         db_df = pd.read_excel(self.database_name, sheet_name=sheet, header=header)
         return db_df
 
-    def create_dataframe_of_samples(self, df, samples):
+    @staticmethod
+    def create_dataframe_of_samples(df, samples):
         samples_search = df.loc[df['Lab ID - DNA'].isin(samples)]
         return samples_search
 
-    def get_sample(self, df, sample):
+    @staticmethod
+    def get_sample(df, sample):
         # Locate sample and return dataframe for that sample (only do this lookup once per sample)
         sample = df.loc[df['Lab ID - DNA'] == sample]
         return sample
 
-    def get_cruk_sample_id(self, df):
+    @staticmethod
+    def get_cruk_sample_id(df):
         # return cruk sample id as a series, convert to string and take value part excluding row number
         cruk_sample_id = " ".join(df["CRUK sample ID"].to_string().split()[1:])
         return cruk_sample_id
 
-    def get_clinical_hub(self, df):
+    @staticmethod
+    def get_clinical_hub(df):
         from valid_data import ch_dict
         # return clinical hub as a series, convert to string and take value part excluding row number
         ch = " ".join(df['Source Clinical Hub'].to_string().split()[1:])
@@ -38,27 +42,32 @@ class ParseDatabase:
         ch_formatted = ch_dict[ch]
         return ch_formatted
 
-    def get_org_code(self, df):
+    @staticmethod
+    def get_org_code(df):
         # return organisation code as a series, convert to string and take value part excluding row number
         org_code = " ".join(df['Hospital Org Code'].to_string().split()[1:])
         return org_code
 
-    def get_patient_id(self, df):
+    @staticmethod
+    def get_patient_id(df):
         # return patient identifier as a series, convert to string and take value part excluding row number
         pat_id = " ".join(df['Local patient ID'].to_string().split()[1:])
         return pat_id
 
-    def get_patient_id_2(self, df):
+    @staticmethod
+    def get_patient_id_2(df):
         # return second patient identifier as a series, convert to string and take value part excluding row number
         pat_id_2 = " ".join(df['Local patient ID 2'].to_string().split()[1:])
         return pat_id_2
 
-    def get_source_sample_id(self, df):
+    @staticmethod
+    def get_source_sample_id(df):
         # return source sample identifier as a series, convert to string and take value part excluding row number
         source_id = " ".join(df['Source Sample ID'].to_string().split()[1:])
         return source_id
 
-    def get_sample_type(self, df):
+    @staticmethod
+    def get_sample_type(df):
         from valid_data import st_dict
         # return sample type as a series, convert to string and take value part excluding row number
         sample_type = " ".join(df['Sample type'].to_string().split()[1:])
@@ -68,7 +77,8 @@ class ParseDatabase:
         st_formatted = st_dict[sample_type]
         return st_formatted
 
-    def get_tumour_type(self, df):
+    @staticmethod
+    def get_tumour_type(df):
         from valid_data import tumour_type_dict
         # return tumour type as a series, convert to string and take value part excluding row number
         tumour_type = " ".join(df['Tumour type'].to_string().split()[1:])
@@ -76,33 +86,38 @@ class ParseDatabase:
         tumour_type_formatted = tumour_type_dict[tumour_type]
         return tumour_type_formatted
 
-    def get_snomed(self, df):
+    @staticmethod
+    def get_snomed(df):
         snomed_code = " ".join(df['SNOMED'].to_string().split()[1:])
         if snomed_code is None or snomed_code.isspace() or len(snomed_code) == 0 or snomed_code == "NaN":
             snomed_code = "N/A"
         return snomed_code
 
-    def get_date_sample_sent(self, df):
+    @staticmethod
+    def get_date_sample_sent(df):
         # return date (without time) received as a series, convert to string and take value part excluding row number
         sent_date = df['Date sample received'].dt.date.to_string()
         # split date to remove row number
         sent_date = " ".join(sent_date.split()[1:])
         return sent_date
 
-    def get_date_sample_received(self, df):
+    @staticmethod
+    def get_date_sample_received(df):
         # return date (without time) received as a series, convert to string and take value part excluding row number
         received_date = df['Date sample sent'].dt.date.to_string()
         # split date to remove row number
         received_date = " ".join(received_date.split()[1:])
         return received_date
 
-    def get_lab_id(self, df):
+    @staticmethod
+    def get_lab_id(df):
         # return DNA identifier as a series, convert to string and take value part excluding row number
         lab_id = " ".join(df['Lab ID - DNA'].to_string().split()[1:])
         lab_id = lab_id.upper() # Handle the case where M may be entered into database as lower case m
         return lab_id
 
-    def get_report_release_date(self, df):
+    @staticmethod
+    def get_report_release_date(df):
         # return date (without time) received as a series, convert to string and take value part excluding row number
         try:
             release_date = df['date reported'].dt.date.to_string()
@@ -112,7 +127,8 @@ class ParseDatabase:
         release_date = " ".join(release_date.split()[1:])
         return release_date
 
-    def get_vol_banked_dna(self, df):
+    @staticmethod
+    def get_vol_banked_dna(df):
         # return vol of banked nucleic acid as a series, convert to string and take value part excluding row number
         vol = " ".join(df['DNA Volume (µL)'].to_string().split()[1:])
         # Where there is no concentration as no nucleic acid was banked, enter 0
@@ -120,7 +136,8 @@ class ParseDatabase:
             vol = "0"
         return vol
 
-    def get_conc_banked_dna(self, df):
+    @staticmethod
+    def get_conc_banked_dna(df):
         # return conc of banked nucleic acid as a series, convert to string and take value part excluding row number
         conc = " ".join(df['Final FFPE DNA conc for NGS'].to_string().split()[1:])
         # Where there is no concentration as no nucleic acid was banked, enter 0
@@ -128,7 +145,8 @@ class ParseDatabase:
             conc = "0"
         return conc
 
-    def get_vol_banked_rna(self, df):
+    @staticmethod
+    def get_vol_banked_rna(df):
         # return vol of banked nucleic acid as a series, convert to string and take value part excluding row number
         vol = " ".join(df['RNA Volume (µL)'].to_string().split()[1:])
         # Where there is no concentration as no nucleic acid was banked, enter 0
@@ -136,7 +154,8 @@ class ParseDatabase:
             vol = "0"
         return vol
 
-    def get_conc_banked_rna(self, df):
+    @staticmethod
+    def get_conc_banked_rna(df):
         # return conc of banked nucleic acid as a series, convert to string and take value part excluding row number
         conc = " ".join(df['Final FFPE RNA conc for NGS'].to_string().split()[1:])
         # Where there is no concentration as no nucleic acid was banked, enter 0
---
+++ tests/test_cruk_report.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestCrukReport(unittest.TestCase):
+
+    def first_test(self):
+        return None
+
+
+if __name__ == '__main__':
+    unittest.main()
