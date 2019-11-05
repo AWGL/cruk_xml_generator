32fa15addc7ecce012f1f790be1c0012dd4517fe
Sara Rey <sara.rey.manchester@gmail.com>
2019-11-05 09:59:46 +0000

    input tests

--- tests/test_cruk_report.py
+++ tests/test_cruk_report.py
@@ -3,8 +3,47 @@ import unittest
 
 class TestCrukReport(unittest.TestCase):
 
-    def first_test(self):
+    # Tests for input
+    def test_status_input(self):
         return None
+
+    def test_sample_input(self):
+        return None
+
+    def test_worksheet_input(self):
+        return None
+
+    def test_authoriser_input(self):
+        return None
+
+
+    # Tests for data parsed out into dictionary
+
+
+    # Tests for XML generated
+    def pass_xml_test(self):
+        '''
+        Test if newly generated xml for a sample which passed QC and was sequenced matches an existing xml
+        :return:
+        '''
+
+        # Load in template xml
+
+
+        # Load in generated xml
+
+
+        return None
+
+    def fail_xml_test(self):
+        return None
+
+    def withdrawn_xml_test(self):
+        return None
+
+
+    # Check PDF is correctly named
+
 
 
 if __name__ == '__main__':
